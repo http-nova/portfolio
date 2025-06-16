546aba4882981ed0bc7c776a0dd507c8a87f11df
Nova <prashantkumawat0823@gmail.com>
2025-06-17 01:46:22 +0530

    Make Home section responsive and improve mobile layout

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,27 +1,37 @@
+import { useState } from "react";
 import "./NavBar.css";
 
 function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
+
   return (
-    <>
-      <div className="nav-main">
-          <ul>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-            <li>
-              <a href="#footer">Contact</a>
-            </li>
-            <li>
-              <div className="nav-box">
-                <a href="#home">Home</a>
-              </div>
-            </li>
-          </ul>
+    <nav className="nav-main">
+      <div className="menu-toggle" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
-    </>
+
+      <div className={`nav-links ${menuOpen ? "open" : ""}`}>
+        <ul>
+          <li>
+            <a href="#about" onClick={closeMenu}>About</a>
+          </li>
+          <li>
+            <a href="#projects" onClick={closeMenu}>Projects</a>
+          </li>
+          <li>
+            <a href="#footer" onClick={closeMenu}>Contact</a>
+          </li>
+          <li className="nav-box">
+            <a href="#home" onClick={closeMenu}>Home</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
